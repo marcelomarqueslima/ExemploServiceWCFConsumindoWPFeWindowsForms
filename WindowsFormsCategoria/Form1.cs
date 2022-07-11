41c7fa3b727ee9f9820c74131c0f4828312643ac
Milton <mjcalmeida@hotmail.com>
2022-07-11 11:29:16 -0300

    Crud

--- WindowsFormsCategoria/Form1.cs
+++ WindowsFormsCategoria/Form1.cs
@@ -16,5 +16,43 @@ namespace WindowsFormsCategoria
         {
             InitializeComponent();
         }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            SVC_Categorias.Service1Client svcCategoria = new SVC_Categorias.Service1Client();
+            string returnString;
+
+            returnString = svcCategoria.getCategoria(Int32.Parse(txtIDCategoria.Text));
+            lblCategoria.Text = returnString;
+        }
+
+        private void lblIncluir_Click(object sender, EventArgs e)
+        {
+            SVC_Categorias.Service1Client svcCategoria = new SVC_Categorias.Service1Client();
+            string returnString;
+
+            returnString = svcCategoria.CriarCategoria(txtIncluir.Text);
+            lblInclusao.Text = returnString;
+            txtIDCategoria.Text = "";
+            txtAlterar.Text = "";
+        }
+
+        private void txtIDCategoria_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            int num = 0;
+            e.Handled = !int.TryParse(e.KeyChar.ToString(), out num);
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            if (txtIDCategoria.TextLength != 0)
+            {
+                SVC_Categorias.Service1Client svcCategoria = new SVC_Categorias.Service1Client();
+                string returnString;
+
+                returnString = svcCategoria.CriarCategoria(txtIncluir.Text);
+                lblInclusao.Text = returnString;
+            }
+        }
     }
 }
